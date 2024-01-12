753c0f2d56d8d39b023c19fa85aab477c096a014
7dvys <fdr160102@gmail.com>
2024-01-12 16:34:25 -0300

    search filter fixed

--- src/utils/tableComponent/passSearchFilter.ts
+++ src/utils/tableComponent/passSearchFilter.ts
@@ -3,8 +3,19 @@ import { TableItem } from "@/types/TableTypes";
 export const passSearchFilter = ({search,searchableColumns,item}:{search:string,searchableColumns:string[],item:TableItem})=>{
     return searchableColumns.some(key=>{
         const itemValues = Array.isArray(item[key])?item[key] as (string|number)[]:[item[key]];
-        return itemValues.some(itemValue=>
-            (itemValue as string|number).toString().toUpperCase().includes(search.toUpperCase())
+        return itemValues.some(itemValue=>{
+            if(search === '' || search === undefined || search === null)
+            return false;
+            
+            if(itemValue === undefined || itemValue === null || itemValue === '')
+            return false;
+
+            if(typeof itemValue === 'string' || typeof itemValue === 'number')
+            return itemValue.toString().toUpperCase().includes(search.toUpperCase())
+
+            return false;
+        }
+            
         )   
     })
 }
